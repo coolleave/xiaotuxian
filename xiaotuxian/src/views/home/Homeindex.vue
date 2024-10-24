c66103a4647fd8621824d0bfb030241b496e97ac
coolleave <cpy2855048477@163.com>
2024-10-24 23:35:33 +0800

    完成产品列表，但排版问题未解决

--- xiaotuxian/src/views/home/Homeindex.vue
+++ xiaotuxian/src/views/home/Homeindex.vue
@@ -5,7 +5,7 @@ import banner from './components/HomeBanner.vue';
 import goods from './components/HomeGoods.vue';
 import hot from './components/HomeHot.vue';
 import product from './components/HomeProduct.vue';
-import homePanel from './components/HomePanel.vue';
+
 </script>
 
 <template>
@@ -19,11 +19,7 @@ import homePanel from './components/HomePanel.vue';
     <goods/>
     <hot/>
     <product/>
-    <homePanel title="新鲜好物" sub-title="新鲜好物 细心选择">
-        <div>
-            我是插槽内容
-        </div>
-    </homePanel>
+    
 </template>
 
 <style scoped>
