e07556472a4e8f3a50394a181815cb33e184ca48
coolleave <cpy2855048477@163.com>
2024-11-18 12:49:19 +0800

    勾选协议校验

--- xiaotuxian/src/views/login/index.vue
+++ xiaotuxian/src/views/login/index.vue
@@ -5,7 +5,8 @@ import { ref } from 'vue';
 // 表单对象
 const form = ref({
     account: '',
-    password: ''
+    password: '',
+    agree:true
 })
 
 // 表单规则
@@ -16,6 +17,17 @@ const rules = {
     password: [
         { required: true, message: '密码不能为空', trigger: 'blur' },
         { max: 14, min: 6, message: '密码要在6-14位之间', trigger: 'blur' }
+    ],
+    agree:[
+        {
+            validator:(rule,value,callback)=>{
+                if(value){
+                    callback()
+                }else{
+                    callback(new Error("请勾选协议"))
+                }
+            }
+        }
     ]
 }
 </script>
@@ -50,8 +62,8 @@ const rules = {
                             <el-form-item prop="password" label="密码">
                                 <el-input v-model="form.password" />
                             </el-form-item>
-                            <el-form-item label-width="22px">
-                                <el-checkbox size="large">
+                            <el-form-item prop="agree"  label-width="22px">
+                                <el-checkbox size="large" v-model="form.agree">
                                     我已同意隐私条款和服务条款
                                 </el-checkbox>
                             </el-form-item>
