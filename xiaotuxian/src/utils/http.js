607f9f8b244f3150c84b2dc99e9bc941c26e9960
coolleave <cpy2855048477@163.com>
2024-11-20 21:01:56 +0800

    登出功能实现

--- xiaotuxian/src/utils/http.js
+++ xiaotuxian/src/utils/http.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import 'element-plus/es/components/message/style/css'
 import {ElMessage}  from 'element-plus'
 
+import { useUserStore } from "@/stores/user";
+
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -12,6 +14,11 @@ const httpInstance = axios.create({
 // 添加请求拦截器
 httpInstance.interceptors.request.use(config => {
     // 在发送请求之前做些什么
+    const userStore = useUserStore() 
+    if(userStore.userInfo.token){
+        const token = userStore.userInfo.token
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config;
 },
     // 对请求错误做些什么
--- xiaotuxian/src/views/layout/components/LayoutNav.vue
+++ xiaotuxian/src/views/layout/components/LayoutNav.vue
@@ -1,6 +1,15 @@
 <script setup>
 import {useUserStore} from  '@/stores/user.js';
+import { useRouter } from 'vue-router';
 const userStore = useUserStore()
+const router = useRouter()
+const confirm=()=>{
+  // 清除store
+  userStore.clearUserInfo()
+  // 路由跳转
+  router.replace('/login')
+  
+}
 </script>
 
 <template>
@@ -10,7 +19,7 @@ const userStore = useUserStore()
         <template v-if="userStore.userInfo.token">
           <li><a href="javascript:;"><i class="iconfont icon-user"></i>{{userStore.userInfo.account}}</a></li>
           <li>
-            <el-popconfirm title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
+            <el-popconfirm @confirm="confirm" title="确认退出吗?" confirm-button-text="确认" cancel-button-text="取消">
               <template #reference>
                 <a href="javascript:;">退出登录</a>
               </template>
