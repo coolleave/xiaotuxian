372b11202bc00cc68313236df4c27c800228ce47
coolleave <cpy2855048477@163.com>
2024-11-22 21:50:59 +0800

    购物车列表渲染

--- xiaotuxian/src/router/index.js
+++ xiaotuxian/src/router/index.js
@@ -5,6 +5,7 @@ import category from '@/views/category/index.vue'
 import subCategory from '@/views/subCategory/index.vue'
 import detail from '@/views/detail/index.vue'
 import login from '@/views/login/index.vue'
+import cartList from '@/views/cartList/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -33,6 +34,10 @@ const router = createRouter({
           path:'detail/:id',
           component:detail
         },
+        {
+          path:'cart',
+          component:cartList
+        }
 
         
 
